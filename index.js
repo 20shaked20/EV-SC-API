abad15be699b5135d3758450036ca6ae48fe1320
Yuval Bubnosky <yuval.bubnov@gmail.com>
2022-12-28 15:47:56 +0200

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const config = require('./config');
+// const barberRoutes = require('./routes/barber-routes)
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+// app.use('/api', barberRoutes.routes);
+
+app.listen(config.port,
+     ()=> console.log('EV-SC application server is listening on localhost:' + config.port));
