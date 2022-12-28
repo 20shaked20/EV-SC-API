d93b80a4a395feca35f142d826fa725d6d2d9830
shaked <shakedlevi32@gmail.com>
2022-12-28 17:46:48 +0200

    Favorite controller -> logic, should work but bugged right now

---
+++ models/favourite.js
@@ -0,0 +1,7 @@
+class Favorite{
+    constructor(SID, name, location) {
+        this.SID = SID;
+        this.name = name;
+        this.location = location;
+    }
+}
---
+++ models/review.js
@@ -0,0 +1,7 @@
+class Review{
+    constructor(UID, rating, data){
+        this.UID = UID;
+        this.rating = rating;
+        this.data = data;
+    }
+}
