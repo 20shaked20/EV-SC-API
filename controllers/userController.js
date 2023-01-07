bc8c59fb2490218063f506412da093b220a3b62f
Yuval Bubnovsky <93248620+YuvalBubnovsky@users.noreply.github.com>
2023-01-07 15:10:11 +0200

    reviews + favorites are now inside stations/users

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,7 +27,61 @@ const getUser = async (req, res, next) => {
     }
 }
 
+const getAllUsers = async (req, res, next) => {
+    try {
+      const users = [];
+      const snapshot = await db.collection("users").get();
+      snapshot.forEach((doc) => {
+        users.push(doc.data());
+      });
+      res.send(users);
+    } catch (err) {
+      res.status(400).send(err.message);
+    }
+  };
+
+  const AddFavorite = async (req, res, next) =>{
+    const uid = req.params.id.substring(1);
+    try{
+        const data = req.body;
+        await db.collection("users")
+        .doc(uid)
+        .collection("favorites")
+        .set(data)
+        res.send("Favorite station added successfully");
+    }catch(err){
+        res.status(400).send(err.message);
+    }
+}
+
+const getFavoriteList = async (req, res, data) => {
+    const uid = req.params.id.substring(1);
+    let favorites = [];
+    await db
+    .collection("users")
+    .doc(uid)
+    .collection("favorites")
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((documentSnapshot) => {
+            const name = documentSnapshot.data().Station;
+            const sid = documentSnapshot.data().SID;
+            const location = documentSnapshot.data().Location;
+
+            favorites.push({ label:name, value:sid, loc:location });
+        });
+    })
+    .catch((err) => {
+        res.status(400).send(err.message)
+    });
+    
+    res.send(favorites);
+}
+
 module.exports = {
     addUser,
-    getUser
+    getUser,
+    getAllUsers,
+    AddFavorite,
+    getFavoriteList
 }
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const {
     addUser,
-    getUser
+    getUser,
+    getAllUsers,
+    AddFavorite,
+    getFavoriteList
 } = require('../controllers/userController');
 
 const router = express.Router();
 
 router.post('/user', addUser);
 router.get('/user/:id', getUser);
+router.get('/users', getAllUsers);
+router.post('/user/:id/favorite',AddFavorite);
+router.get('/user/:id/favorites', getFavoriteList);
+
 
 module.exports = {
     routes: router
