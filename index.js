96615aa6b766349c9f6861919544eb8ceaef8493
shaked <shakedlevi32@gmail.com>
2023-01-08 12:23:56 +0200

    bla

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const config = require('./config');
 
-const reviewRoutes = require('./routes/review-routes');
-const favoriteRoutes = require('./routes/favourite-routes');
 const userRoutes = require('./routes/user-routes');
 const stationRoutes = require('./routes/station-routes');
 
@@ -15,8 +13,6 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/api', favoriteRoutes.routes);
-app.use('/api', reviewRoutes.routes);
 app.use('/api', userRoutes.routes);
 app.use('/api', stationRoutes.routes);
 
